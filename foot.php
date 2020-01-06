1f4c5626c17efa61b42e1c172b6ab37325119f8c
bayuagung100 <bayuagung100@gmail.com>
2020-01-06 20:13:47 +0700

    tambah histats

--- foot.php
+++ foot.php
@@ -10,18 +10,18 @@
 		
 		<center>
 		    <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-            <!-- Histats.com  START  (aync)-->
-            <script type="text/javascript">var _Hasync= _Hasync|| [];
-            _Hasync.push(['Histats.start', '1,4208606,4,233,112,50,00010100']);
-            _Hasync.push(['Histats.fasi', '1']);
-            _Hasync.push(['Histats.track_hits', '']);
-            (function() {
-            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-            hs.src = ('//s10.histats.com/js15_as.js');
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-            })();</script>
-            <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4208606&101" alt="" border="0"></a></noscript>
-            <!-- Histats.com  END  -->
+			<!-- Histats.com  START  (aync)-->
+			<script type="text/javascript">var _Hasync= _Hasync|| [];
+			_Hasync.push(['Histats.start', '1,4355590,4,227,112,50,00010100']);
+			_Hasync.push(['Histats.fasi', '1']);
+			_Hasync.push(['Histats.track_hits', '']);
+			(function() {
+			var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+			hs.src = ('//s10.histats.com/js15_as.js');
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+			})();</script>
+			<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4355590&101" alt="" border="0"></a></noscript>
+			<!-- Histats.com  END  -->
 		</center>
 		
 		
