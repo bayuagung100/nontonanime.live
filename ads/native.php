c0eea4ca6ee5ce6d23be5cadfd1b09b0e6d1355a
bayuagung100 <bayuagung100@gmail.com>
2020-01-14 18:34:29 +0700

    tambah iklan adskeper

---
+++ ads/native.php
@@ -0,0 +1,9 @@
+<style>#M518300ScriptRootC785416 {min-height: 300px;}</style>
+<!-- Composite Start -->
+    <div id="M518300ScriptRootC785416"> 
+        <div id="M518300PreloadC785416"> Loading... 
+        </div> 
+        </div>
+        <script src="//jsc.adskeeper.co.uk/n/o/nontonanime.live.785416.js" async>
+        </script>
+<!-- Composite End -->
