86cf7fa9b5ab7fa4f943e8d1139ce674afadab22
bayuagung100 <bayuagung100@gmail.com>
2020-02-23 12:35:55 +0700

    tambah banner adz2you

--- ads/468.php
+++ ads/468.php
@@ -4,3 +4,15 @@
 <div class="col-sm-6 text-center potong">
     <iframe data-aa="1178948" src="//ad.a-ads.com/1178948?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
 </div>
+
+<div class="col-sm-6 text-center potong">
+    <!--[ 468x60 Ad Tag ( nontonanime.live ) - Adz2you.net // www.adz2you.net // ]-->
+    <iframe src="//adz2you.net/serve/show.php?a=5282&b=468x60" width="468" height="60" frameBorder="0" scrolling="no" marginwidth="0" marginheight="0"></iframe>
+    <!--[ End of the Ad Tag - Adz2you.net // www.adz2you.net // ]-->
+</div>
+<div class="col-sm-6 text-center potong">
+    <!--[ 468x60 Ad Tag ( nontonanime.live ) - Adz2you.net // www.adz2you.net // ]-->
+    <script type="text/javascript"> var a = "5282"; var b = "468x60"; var network = "adz2you.net"; </script>
+    <script type="text/javascript" src="https://www.adz2you.net/serve/ads.js"></script>
+    <!--[ End of the Ad Tag - Adz2you.net // www.adz2you.net // ]-->
+</div>
